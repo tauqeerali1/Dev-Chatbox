9340e9c20087e469b6bdeda20923972d5f5949c3
Tauqeer Ali <tauqeer.ali.9934@gmail.com>
2020-09-17 00:43:18 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,10 +2,10 @@
 $servername   = "";
 $database = "";
 $username = "";
-$Password = "";
+$password = "";
 
 // Create connection
-$con = new mysqli($servername, $username, $Password, $database);
+$con = new mysqli($servername, $username, $password, $database);
 // Check connection
 if ($con->connect_error) {
    die("Connection failed: " . $con->connect_error);
